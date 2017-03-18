3fe2e5f9e7c1b85c34a0b33384b150319e919b91
Paulo Henrique Leite <paulo-http@live.com>
2017-03-18 19:34:54 -0300

    adicionado matrix_confusion e exibindo dados

--- knn.py
+++ knn.py
@@ -39,23 +39,60 @@ def find_neighbors(test, rest, k):
 		neighbors.append(distances[x][0])
 	return neighbors
 
-def main():
-	# prepare data
-	array = load_csv_file('iris', 4)
+def matrix_confusion(test, neighbors, matrix):
+	error = 0.0
+	lenght = 0.0
+	for x in range(len(neighbors)):
+		lenght = len(neighbors[x])
+		for y in range(len(neighbors[x])):			
+			actual = test[y][-1]
+			predicted = neighbors[x][y][-1]
+			current = actual + " | " + predicted
+			mark_class(current, matrix)
+			error += mark_error(actual, predicted)
+	return error
 
-	# prepare 10-fold-cross validation
-	validation = []
-	for x in xrange(0,10):
-		validation.append(cross_validation(array, x))
-	
-	# generate predictions
-	k = 1
-	for x in xrange(0,10):
-		test = validation[x]
-		rest = list(validation)
-		rest.pop(x)
-		neighbors = find_neighbors(test, rest, k)
-		print("test: %s" % (validation[x]))
-		print("neighbors: %s\n" % (neighbors))
+def mark_class(current, matrix):
+	if current in matrix:
+		matrix[current] += 1			
+	else:
+		matrix[current] = 1
+
+def mark_error(actual, predicted):
+	if actual == predicted:
+		return 0
+	else:
+		return 1
+
+def main():
+	# prepare config
+	# config = [['iris', 4, 3], ['adult', 14, 2], ['wine', 13, 3], ['cancer', 9, 2], ['quality', 11, 11], ['abalone', 8, 29]]
+	config = [['iris', 4, 3]]
+
+	for x in range(len(config)):
+		# prepare data
+		filename = config[x][0]
+		attrs = config[x][1]
+		types = config[x][2]
+		csv = load_csv_file(filename, attrs)
+
+		# prepare 10-fold-cross validation
+		validation = []
+		for x in xrange(0,10):
+			validation.append(cross_validation(csv, x))
+				
+		# generate predictions and create matrix of confusion
+		k = 1		
+		error = {}
+		matrix = {}
+		for x in xrange(0,10):
+			test = validation[x]
+			rest = list(validation)
+			rest.pop(x)
+			neighbors = find_neighbors(test, rest, k)
+			key = "error p_" + str(x+1)
+			error[key] = matrix_confusion(test, neighbors, matrix)				
+		print matrix
+		print error
 
 main()
