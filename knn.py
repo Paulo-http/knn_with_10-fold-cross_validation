d77f79cbd29d01b7256b246fd4abc7dc91cc530a
Paulo Henrique Leite <paulo-http@live.com>
2017-03-18 14:55:00 -0300

    adicionado euclidean_distance e find_neighbors e exibindo resultado

--- knn.py
+++ knn.py
@@ -21,6 +21,24 @@ def cross_validation(data, times):
 		array.append(data[x+len(lenght)*times])
 	return array
 
+def euclidean_distance(test, rest, length):
+	distance = 0
+	for x in range(length):
+		for y in range(len(test[x])-1):
+			distance += pow((test[x][y] - rest[x][y]), 2)
+	return math.sqrt(distance)
+
+def find_neighbors(test, rest, k):
+	distances = []
+	for x in range(len(rest)):
+		result = euclidean_distance(test, rest[x], len(test))
+		distances.append((rest[x], result))
+	distances.sort(key=operator.itemgetter(1))
+	neighbors = []
+	for x in range(k):
+		neighbors.append(distances[x][0])
+	return neighbors
+
 def main():
 	# prepare data
 	array = load_csv_file('iris', 4)
@@ -29,6 +47,15 @@ def main():
 	validation = []
 	for x in xrange(0,10):
 		validation.append(cross_validation(array, x))
-	print validation
 	
-main()	
+	# generate predictions
+	k = 1
+	for x in xrange(0,10):
+		test = validation[x]
+		rest = list(validation)
+		rest.pop(x)
+		neighbors = find_neighbors(test, rest, k)
+		print("test: %s" % (validation[x]))
+		print("neighbors: %s\n" % (neighbors))
+
+main()
