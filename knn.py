390c50bbde097ef507178c7efb32acb1259e355e
Paulo Henrique Leite <paulo-http@live.com>
2017-03-18 14:34:28 -0300

    criado knn.py com função para carregar um csv

---
+++ knn.py
@@ -0,0 +1,15 @@
+import csv
+import random
+import math
+import operator
+
+def load_csv_file(filename, attr):
+	array = []
+	with open(filename, 'rb') as csvfile:
+	    lines = csv.reader(csvfile)
+	    data = list(lines)
+	    for x in range(len(data)):
+	        for y in range(attr):
+	            data[x][y] = float(data[x][y])
+	        array.append(data[x])
+	return array
